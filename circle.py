c7aa6a4b95d427a97c281c19c86b397c092ce63c
Serios7 <Serios601@gmail.com>
2017-11-18 10:09:20 +1000

    INIT

--- circle.py
+++ circle.py
@@ -1,3 +1,11 @@
+import figure
 
+
+class circle(figure.figure):
+    def perimeter(self):
+        pass
+
+    def area(self):
+        pass
 
 
---
+++ triangle.py
@@ -0,0 +1,9 @@
+import figure
+
+
+class triangle(figure.figure):
+    def perimeter(self):
+        pass
+
+    def area(self):
+        pass
